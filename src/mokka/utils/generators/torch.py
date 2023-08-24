1995cc853a08e2e74831a66cee14a82736205f35
Andrej Rode <rode@kit.edu>
2023-08-24 13:58:17 +0200

    generators: Add generate_bits to generators

--- src/mokka/utils/generators/torch.py
+++ src/mokka/utils/generators/torch.py
@@ -1,3 +1,14 @@
 """Module for data generators in PyTorch."""
 
 import torch  # noqa
+
+
+def generate_bits(shape):
+    """Generate uniform random bits.
+
+    :param shape: tuple with resulting shape
+    :returns: array with uniform random bits in the requested shape
+
+    """
+    bits = torch.randint(0, 2, shape)
+    return bits
