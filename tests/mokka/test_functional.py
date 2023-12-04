c360862f02227283073f68cbe590d93d6e611ebe
Andrej Rode <rode@kit.edu>
2023-12-04 16:59:22 +0100

    tests: Add tests for convolve_overlap_save

--- tests/mokka/test_functional.py
+++ tests/mokka/test_functional.py
@@ -27,6 +27,32 @@ def test_convolve_pytorch_complex():
     assert np.allclose(reference, result)
 
 
+def test_convolve_overlap_save_pytorch_complex():
+    a = np.arange(10) + 1j * np.arange(10)
+    b = np.arange(3) - 1j * np.arange(3)
+    reference = np.convolve(a, b)
+    result = (
+        mokka.functional.torch.convolve_overlap_save(torch.tensor(a), torch.tensor(b))
+        .detach()
+        .numpy()
+    )
+    assert np.allclose(reference, result)
+
+
+def test_convolve_overlap_save_full_pytorch_complex_long():
+    a = np.random.standard_normal(1000) + 1j * np.random.standard_normal(1000)
+    b = np.random.standard_normal(10) - 1j * np.random.standard_normal(10)
+    reference = np.convolve(a, b, "full")
+    result = (
+        mokka.functional.torch.convolve_overlap_save(
+            torch.tensor(a), torch.tensor(b), "full"
+        )
+        .detach()
+        .numpy()
+    )
+    assert np.allclose(reference, result)
+
+
 def test_unwrap():
     angle_diff = np.random.random(10000) * 2 * np.pi
     angles = np.cumsum(angle_diff)
