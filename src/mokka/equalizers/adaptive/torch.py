solved issue with butterflyNxN in backprop by defining second Conv1D layer. minor fixes.

src/mokka/equalizers/adaptive/torch.py
@@ -363,7 +363,7 @@ def ELBO_NxN(
         h_conv_E_Q_x.real**2
         + h_conv_E_Q_x.imag**2
         + butterfly_filter.forward_abssquared(
-            (E_Q_x_abssq - torch.abs(E_Q_x) ** 2).to(torch.float32)
+            (E_Q_x_abssq - torch.abs(E_Q_x) ** 2)#.to(torch.float32)
         ),
         axis=1,
     )

src/mokka/equalizers/torch.py
@@ -214,7 +214,7 @@ class ButterflyNxN(torch.nn.Module):
             for diag_filter in range(num_channels):
                 filter_taps[
                     diag_filter * num_channels, num_taps // 2
-                ] = 1.0  # filter h_ij with i=j (diagonal of MIMO matrix) are initialized with Dirac
+                ] = 1.0+1j*0  # filter h_ij with i=j (diagonal of MIMO matrix) are initialized with Dirac
             self.num_taps = num_taps
         else:
             assert (taps.dim() == 2 and taps.size()[0] == num_channels**2) or (
@@ -241,6 +241,13 @@ class ButterflyNxN(torch.nn.Module):
                 padding=mode,
                 bias=False,
             )  # convolve
+            self.convolve_sq = torch.nn.Conv1d(
+                self.num_channels,
+                self.num_channels,
+                kernel_size=self.num_taps,
+                padding=mode,
+                bias=False,
+            )  # convolve squarred filter
         else:
             raise ValueError(
                 "Not yet implemented for frequency domain."
@@ -258,7 +265,7 @@ class ButterflyNxN(torch.nn.Module):
         assert y.dtype == torch.complex64
 
         self.convolve.weight.data = torch.flip(
-            self.taps.reshape(8, 8, 25), (2,)
+            self.taps.reshape(self.num_channels, self.num_channels, self.num_taps), (2,)
         )  # flip to assre proper convolution (not cross-correlation) - not neccessary for symmetric filters
         return torch.squeeze(self.convolve(y))
 
@@ -274,10 +281,10 @@ class ButterflyNxN(torch.nn.Module):
         # assert y.dtype == torch.complex64
 
         h_abs_sq = self.taps.real**2 + self.taps.imag**2
-        self.convolve.weight.data = torch.flip(
-            h_abs_sq.reshape(8, 8, 25), (2,)
+        self.convolve_sq.weight.data = torch.flip(
+            h_abs_sq.reshape(self.num_channels, self.num_channels, self.num_taps), (2,)
         )  # flip to assre proper convolution (not cross-correlation) - not neccessary for symmetric filters
-        return torch.squeeze(self.convolve(y))
+        return torch.squeeze(self.convolve_sq(y))
 
 
 class Butterfly4x4(torch.nn.Module):