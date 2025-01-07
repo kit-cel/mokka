8fc57a99329f55a931fec5b97ffd7e28adb99f58
Andrej Rode <rode@kit.edu>
2025-01-07 16:52:40 +0100

    docs: Automatically document __init__() members of classes

--- docs/conf.py
+++ docs/conf.py
@@ -14,3 +14,11 @@ config = SphinxConfig("../pyproject.toml", globalns=globals(), style="poetry")
 
 author=config.author
 project=config.name
+
+def skip(app, what, name, obj, would_skip, options):
+    if name == "__init__":
+        return False
+    return would_skip
+
+def setup(app):
+    app.connect("autodoc-skip-member", skip)
