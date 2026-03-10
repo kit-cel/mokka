61b38882a54b4bdda4c68d195fdc19d6b19b0ba1
Andrej Rode <rode@kit.edu>
2026-03-10 13:07:10 +0100

    generators: Add PRBS Generator class

--- src/mokka/utils/generators/numpy.py
+++ src/mokka/utils/generators/numpy.py
@@ -14,7 +14,6 @@ def generate_BPSK(N, P_in_dbm):
     :param N: Number of BPSK symbols to generate
     :param P_in_dbm: signal power [dBm]
     :returns: (pseudo-)randomly generated array of BPSK symbols
-
     """
     P_in = 10 ** (P_in_dbm / 10 - 3)
     symbols = np.array([-1, 1]) * np.sqrt(P_in)
@@ -27,7 +26,6 @@ def generate_bits(shape):
 
     :param shape: tuple with resulting shape
     :returns: array with uniform random bits in the requested shape
-
     """
     bits = np.random.choice([0, 1], shape)
     return bits
@@ -38,10 +36,88 @@ def generate_all_bits(m):
 
     :param m: length of the bitstring
     :returns: array with all possible bitstrings.
-
     """
     all_bits = np.arange(2**m, dtype=np.uint8)
     all_bits = np.expand_dims(all_bits, 1)
     # Generate bits from 0 to 2**m
     B = np.flip(np.unpackbits(all_bits, axis=1, count=m, bitorder="little"), axis=1)
     return B
+
+
+def pnsequence(pn_order, pn_seed, pn_mask, seq_length):
+    """Generate a PN (Pseudo-Noise) sequence using a Linear Feedback Shift Register (LFSR).
+
+    Seed and mask are ordered so that:
+        - seed[-1] will be the first output
+        - the new bit computed as :math:`sum(shift_register & mask) % 2` is inserted in shift[0]
+
+    :param pn_order: Number of delay elements used in the LFSR
+    :param pn_seed: Seed for the initialization of the LFSR delay elements
+    :param pn_mask: Mask representing which delay elements contribute to the feedback in the LFSR
+    :param seq_length: Length of the PN sequence to be generated
+    :returns: PN sequence generated and final state of the shift register
+    :raises ValueError: If the pn_order is not equal to the length of the strings pn_seed and pn_mask
+    """
+    # Check if pn_order is equal to the length of the strings 'pn_seed' and 'pn_mask'
+    if len(pn_seed) != pn_order:
+        raise ValueError("pn_seed has not the same length as pn_order")
+    if len(pn_mask) != pn_order:
+        raise ValueError("pn_mask has not the same length as pn_order")
+
+    # Pre-allocate memory for output
+    pnseq = np.empty(seq_length, np.uint8)
+
+    # Convert input as array
+    sr = np.fromiter(pn_seed, np.uint8, pn_order)
+    mask = np.fromiter(pn_mask, np.uint8, pn_order)
+
+    for i in range(seq_length):
+        pnseq[i] = sr[-1]
+        new_bit = np.sum(sr & mask) % 2
+        sr[1:] = sr[:-1]
+        sr[0] = new_bit
+
+    return pnseq, sr
+
+
+prbs_poly = {
+    9: [0, 0, 0, 0, 1, 0, 0, 0, 1],
+    11: [0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 1],
+    15: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 1],
+    20: [0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1],
+    23: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 1],
+    31: 27
+    * [
+        0,
+    ]
+    + [1, 0, 0, 1],
+}
+
+
+class PRBSGenerator:
+    def __init__(self, order, seed=None):
+        """Construct PRBSGenerator.
+
+        :param order: Order of the PRBS generator
+        :param seed: Seed for the PRBS generator
+        """
+        self.order = order
+        if seed == None:
+            self.state = np.ones(order, dtype=np.uint8)
+        else:
+            if seed.shape[0] != order:
+                raise ValueError(
+                    "PRBS seed is required to be same length as PRBS order"
+                )
+            self.state = seed
+        self.mask = prbs_poly[order]
+
+    def __call__(self, length):
+        """Generate PRBS sequence.
+
+        :param length: Length of the PRBS sequence to generate
+        :returns: PRBS sequence
+        """
+        bits, state = pnsequence(self.order, self.state, self.mask, length)
+        self.state = state
+        return bits
